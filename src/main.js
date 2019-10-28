ade0c87f2c531de4c79654055480cbb8955e4ed3
65style <lym210119@gmail.com>
2019-10-28 14:57:51 +0800

    fix baseImgUrl

--- src/main.js
+++ src/main.js
@@ -9,11 +9,12 @@ Vue.config.productionTip = false
 
 App.mpType = 'app'
 // 打包后改
-if (process.env.NODE_ENV === 'production') {
-  Vue.prototype.imgBaseUrl = 'http://39.100.129.13'
-} else {
-  Vue.prototype.imgBaseUrl = 'http://192.168.10.208:8099'
-}
+Vue.prototype.imgBaseUrl = window.location.origin
+// if (process.env.NODE_ENV === 'production') {
+//   Vue.prototype.imgBaseUrl = 'http://39.100.129.13'
+// } else {
+//   Vue.prototype.imgBaseUrl = 'http://192.168.10.208:8099'
+// }
 Vue.prototype.$store = store
 
 Vue.use(MinRequest)
